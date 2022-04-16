1eb94cce2603cf7954d913ecb9e22c272885d832
2005artem2005 <103824573+2005artem2005@users.noreply.github.com>
2022-04-16 16:03:18 +0500

    Update Untitled-1 — копия.py

--- Untitled-1 — копия.py
+++ Untitled-1 — копия.py
@@ -1,1 +1,27 @@
-print('Hello,world')
+from PyQt5.QtWidgets import QLabel, QWidget, QApplication, QVBoxLayout, QHBoxLayout, QPushButon, QLineEdit
+from PyQt5.QtCore import Qt
+
+class MainWin(QWidget):
+    def __init__(self):
+        self.set_appear()
+        self.ininUI()
+        self.connects()
+        self.show()
+
+    def set_appear(self):
+        self.resize(600,400)
+        self.setWindowTitle('Инструкция')
+        self.move(250,250)
+
+    def ininUI(self):
+        hello = QLabel('здесь будет приветствие')
+        instr = QLabel('здесь будет инструкция')
+        button = QPushButon('начать')
+        v_line = QVBoxLayout
+    def connects(self):
+        pass
+
+if __name__ == '__main__':
+    app = QApplication([])
+    main = MainWin()
+    app.exec_()
